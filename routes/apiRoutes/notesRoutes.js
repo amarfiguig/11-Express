8b9f29c145adf9129e4a65c75947e0417385c34e
Amar <69308291+amarfiguig@users.noreply.github.com>
2023-02-18 21:25:32 +0000

    Update notesRoutes.js

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,72 +1,33 @@
-const router = require("express").Router();
-const fs = require("fs");
-const uuidv1 = require("uuidv1");
-const { validateNote } = require("../../lib/notes");
+const tips = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-
-/* GET */
-router.get("/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", function (err, contents) {
-    var words = JSON.parse(contents);
-    res.send(words);
-  });
+// GET Route for retrieving all the tips
+tips.get('/', (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
 });
 
-/* POST */
-router.post("/notes", (req, res) => {
-  if (!validateNote(req.body)) {
-    res.status(400).send("The note is not properly formatted.");
-  } else {
-    fs.readFile("db/db.json", (err, data) => {
-      // Check for error
-      if (err) throw err;
-      // Handle data gathering for json update
-      let notes = JSON.parse(data);
-      req.body.id = notes.length.toString();
-      let note = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv1(),
-      };
-      console.log(note);
-      res.json(note);
-      // Add data to existing json array
-      notes.push(note);
-      console.log(notes);
+// POST Route for a new UX/UI tip
+tips.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+  console.log(req.body);
 
-      // Write updated json to array
-      fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
-        // Check for error
-        if (err) throw err;
-        console.log("OK");
-      });
-    });
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      username,
+      tip,
+      topic,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newTip, './db/tips.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
   }
 });
 
-/* DELETE REQUEST */
-router.delete("/notes/:id", (req, res) => {
-  fs.readFile("db/db.json", (err, data) => {
-    // Check for error
-    if (err) throw err;
-    let deleteId = req.params.id;
-    console.log(deleteId);
-    // Handle data gathering for json update
-    let notes = JSON.parse(data);
-    notes.forEach((item, i) => {
-      if (item.id == deleteId) {
-        console.log(item);
-        notes.splice(i, 1);
-      }
-    });
-
-    // Write updated json to array
-    fs.writeFile("db/db.json", JSON.stringify(notes, null, 2), (err) => {
-      // Check for error
-      if (err) throw err;
-      res.send("200");
-    });
-  });
-});
-
-module.exports = router;
+module.exports = tips;
